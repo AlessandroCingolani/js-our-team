c06be9de6a42f8ce3770a6edbf406f3379de9e0f
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-09-21 14:29:09 +0200

    [add]:array ourTeam with objects

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// Create array with objects
+
+const ourTeam = [
+  {
+    name:'Wayne Barnett',
+    role:'Founder & CEO',
+    imgProfile:'wayne-barnett-founder-ceo.jpg'
+  },
+  {
+    name:'Angela Caroll',
+    role:'Chief Editor',
+    imgProfile:'angela-caroll-chief-editor.jpg'
+  },
+  {
+    name:'Walter Gordon',
+    role:'Office Manager',
+    imgProfile:'walter-gordon-office-manager.jpg'
+  },
+  {
+    name:'Angela Lopez',
+    role:'Social Media Manager',
+    imgProfile:'angela-lopez-social-media-manager.jpg'
+  },
+  {
+    name:'Scott Estrada',
+    role:'Developer',
+    imgProfile:'scott-estrada-developer.jpg'
+  },
+  {
+    name:'Barbara Ramos',
+    role:'Graphic Designer',
+    imgProfile:'barbara-ramos-graphic-designer.jpg'
+  }
+];
